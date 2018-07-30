eb268adf6c5f4cf0603ba61ad7c9a37b307bd5b6
AnkitGourav <Ankitgourav035@gmail.com>
2018-07-30 16:50:16 +0530

    remove TW_EXCLUDE_SUPERSU flag
    Change-Id: Iba2c9201d5bbbee0080c20b2721d73b9de412216

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,6 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
